cbae472b29a3e47d40f2a0bc186d590b2b89c4c6
kaigidwani <kpg8244@rit.edu>
2025-09-16 16:40:05 -0400

    Adde comment

--- TheGoodTheBadAndTheIntoxicated.cs
+++ TheGoodTheBadAndTheIntoxicated.cs
@@ -10,6 +10,6 @@ namespace TheGoodTheBadAndTheIntoxicated
 	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
 	public class TheGoodTheBadAndTheIntoxicated : Mod
 	{
-
+		// bla bla bla
 	}
 }
