dc01386f750bbcf56d151e4dd23f568752b89f3b
yort098 <trham098@gmail.com>
2025-10-03 20:24:41 -0400

    Dungeon generation now uses control points to move spawns
    Placing a honey block in TEdit acts as the player's spawnpoint in the dungeon. Placing a bubblegum block in TEdit changes the surface level of the dungeon.

--- TheGoodTheBadAndTheIntoxicated.cs
+++ TheGoodTheBadAndTheIntoxicated.cs
@@ -29,6 +29,17 @@ namespace TheGoodTheBadAndTheIntoxicated
     /// </summary>
 	public class BarSubworld : Subworld
     {
+        const string DUNGEON_FILEPATH = "Content/Structures/DungeonMVP";
+        // in TEdit, i'll have to mark the surface level and spawnpoints with bubblegum and
+        // honey blocks, respectively
+        const ushort SURFACE_MARKER = TileID.BubblegumBlock; 
+        const ushort SPAWNPOINT_MARKER = TileID.HoneyBlock;
+
+        Mod _modRef;
+        Point16 _dungeonDimensions;
+        Point16 _dungeonOrigin;
+        Dictionary<string, Point16> _poi;
+
         public override int Width => 1000;
         public override int Height => 1000;
 
@@ -40,28 +51,105 @@ namespace TheGoodTheBadAndTheIntoxicated
             new BarGenPass()
         };
 
-        // Sets the time to the middle of the day whenever the subworld loads
+        public BarSubworld() : base()
+        {
+            _modRef = ModLoader.GetMod("TheGoodTheBadAndTheIntoxicated");
+            _dungeonDimensions = StructureHelper.API.Generator.GetStructureDimensions(DUNGEON_FILEPATH, _modRef);
+            _poi = new Dictionary<string, Point16>();
+        }
+
+        /// <summary>
+        /// Spawns the dungeon and player in an appropriate location in the subworld
+        /// </summary>
         public override void OnLoad()
         {
             Main.dayTime = true;
             Main.time = 27000;
 
-            const string DUNGEON_FILEPATH = "Content/Structures/DungeonMVP";
-            Mod modRef = ModLoader.GetMod("TheGoodTheBadAndTheIntoxicated");
-
-            Point16 dungeonDimensions = StructureHelper.API.Generator.GetStructureDimensions(DUNGEON_FILEPATH, modRef);
-            Point16 dungeonOrigin = new Point16((Main.maxTilesX / 2) - (dungeonDimensions.X / 2),
-                (Main.maxTilesY / 2) - (dungeonDimensions.Y / 2));
-            
-            /* --- i need to figure out a way to spawn the player at a specific tile ---
-            int spawnOffsetX = 5; 
-            int spawnOffsetY = 10;
-            Point16 playerSpawn = new Point16(dungeonOrigin.X + spawnOffsetX, dungeonOrigin.Y + spawnOffsetY); */
-
+            // Generating a dummy dungeon to locate the exact spawnpoints and where the structure
+            // needs to be placed to avoid manually inputting these values
+            _dungeonOrigin = new Point16(Main.maxTilesX / 2 - (_dungeonDimensions.X/2)
+                , Main.maxTilesY / 2);
             StructureHelper.API.Generator.GenerateStructure(DUNGEON_FILEPATH,
-                dungeonOrigin, modRef);
+                _dungeonOrigin, _modRef);
 
-            // Main.LocalPlayer.Teleport(new Vector2(playerSpawn.X * 16, playerSpawn.Y * 16));
+            _poi = FindPOI();
+
+            // Spawning the structure in the correct location
+            _dungeonOrigin = new Point16(Main.maxTilesX / 2,
+                (int)Main.worldSurface - _poi["SURFACE_MARKER"].Y);
+            StructureHelper.API.Generator.GenerateStructure(DUNGEON_FILEPATH,
+               _dungeonOrigin, _modRef);
+
+            CleanUpMarkers();
+;
+            // Changing the player's spawn location
+            Main.spawnTileX = _dungeonOrigin.X + _poi["SPAWNPOINT_MARKER"].X;
+            Main.spawnTileY = _dungeonOrigin.Y + _poi["SPAWNPOINT_MARKER"].Y;
+            Main.LocalPlayer.Spawn(PlayerSpawnContext.SpawningIntoWorld);
+        }
+
+        /// <summary>
+        /// Locates all unique points of interest within the given dungeon
+        /// </summary>
+        /// <returns>The point, relative to the dungeon's origin of all POIs
+        /// Note: Since the points are relative to the dungeon's origin, all points
+        ///       can be considered offsets.
+        /// </returns>
+        private Dictionary<string,Point16> FindPOI()
+        {
+            Dictionary<string, Point16> poi = new Dictionary<string, Point16>();
+            
+            for (int x = _dungeonOrigin.X; x < _dungeonOrigin.X + _dungeonDimensions.X; x++)
+            {
+                for (int y = _dungeonOrigin.Y; y < _dungeonOrigin.Y + _dungeonDimensions.Y; y++)
+                {
+                    Tile tile = Main.tile[x, y];
+                    ushort type = tile.TileType;
+
+                    if (type == SURFACE_MARKER || type == SPAWNPOINT_MARKER)
+                    {
+                        string key = tile.TileType == SURFACE_MARKER ? "SURFACE_MARKER" : "SPAWNPOINT_MARKER";
+                        poi.Add(key, new Point16(x - _dungeonOrigin.X, y - _dungeonOrigin.Y));
+                    }
+
+                    WorldGen.KillTile(x, y);
+                }
+            }
+
+            return poi;
+        }
+
+        /// <summary>
+        /// Removes the placeholder markers, and replaces them with an appropriate tile
+        /// </summary>
+        private void CleanUpMarkers()
+        {
+            foreach (KeyValuePair<string, Point16> key in _poi)
+            {
+                Point16 markerLoc = new Point16(
+                    _dungeonOrigin.X + key.Value.X,
+                    _dungeonOrigin.Y + key.Value.Y);
+               
+                // Note that in TEdit, the marker has to be placed
+                // next to at least one tile that it should be replaced with
+                Point16 neighbor = Main.tile[markerLoc.X + 1, markerLoc.Y].HasTile
+                    ? new Point16(markerLoc.X + 1, markerLoc.Y)
+                    : new Point16(markerLoc.X - 1, markerLoc.Y);
+
+                Tile marker = Main.tile[markerLoc.X, markerLoc.Y];
+                Tile source = Main.tile[neighbor.X, neighbor.Y];
+
+                // Copy properties from the neighbor tile to the marker
+                marker.TileType = source.TileType;
+                marker.HasTile = source.HasTile;
+                marker.TileFrameX = source.TileFrameX;
+                marker.TileFrameY = source.TileFrameY;
+                marker.WallType = source.WallType;
+                marker.Slope = source.Slope;
+                marker.IsHalfBlock = source.IsHalfBlock;
+
+            }
         }
     }
 
@@ -77,16 +165,26 @@ namespace TheGoodTheBadAndTheIntoxicated
         protected override void ApplyPass(GenerationProgress progress, GameConfiguration configuration)
         {
             progress.Message = "Generating terrain"; // Sets the text displayed for this pass
-            Main.worldSurface = Main.maxTilesY - 42; // Hides the underground layer just out of bounds
-            Main.rockLayer = Main.maxTilesY; // Hides the cavern layer way out of bounds
+            Main.worldSurface = Main.maxTilesY * 0.25; 
+            Main.rockLayer = Main.maxTilesY * 0.35; 
             for (int i = 0; i < Main.maxTilesX; i++)
             {
                 for (int j = 0; j < Main.maxTilesY; j++)
                 {
                     progress.Set((j + i * Main.maxTilesY) / (float)(Main.maxTilesX * Main.maxTilesY)); // Controls the progress bar, should only be set between 0f and 1f
+
                     Tile tile = Main.tile[i, j];
-                    tile.HasTile = true;
-                    tile.TileType = TileID.Sandstone;
+                    
+                    if (j >= Main.rockLayer)
+                    {
+                        tile.HasTile = true;
+                        tile.TileType = TileID.HardenedSand;
+                    }
+                    else if (j >= Main.worldSurface)
+                    {
+                        tile.HasTile = true;
+                        tile.TileType = TileID.Sand;
+                    }
                 }
             }
 
