129a819d84ad39701764e118fb0fcf0bc4ea71cc
Hyunwoo Park <hp5817@g.rit.edu>
2025-10-07 17:36:39 -0400

    test projectile added. No other entity uses it yet.

---
+++ Content/Projectiles/BulletTest.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TheGoodTheBadAndTheIntoxicated.Content.Projectiles
+{
+    internal class BulletTest : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 5; // The length of old position to be recorded
+            ProjectileID.Sets.TrailingMode[Projectile.type] = 0; // The recording mode        }
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 8;
+            Projectile.height = 8;
+            Projectile.aiStyle = 0;  // Projectile AI style used for bullets
+            Projectile.friendly = false;  // false means it cannot hit enemies.
+            Projectile.hostile = true;  // true means it can hit the player.
+            Projectile.DamageType = DamageClass.Ranged;  // This projectile is shoot by a ranged weapon.
+            Projectile.penetrate = 0;  // This projectile can penetrate 0 enemies.
+            Projectile.timeLeft = 300;  // This projectile lasts for 5 seconds.
+            Projectile.alpha = 255;  // Transparency;
+            Projectile.light = 0.5f;  // The amount of light emitted around the projectile.
+            Projectile.ignoreWater = true;  // Its speed is not affected by the water.
+            Projectile.tileCollide = true;
+            Projectile.extraUpdates = 0;  // Update the projectile multiple time in a frame.
+
+            AIType = ProjectileID.Bullet;  // Default bullet AI
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            Projectile.penetrate--;
+            if (Projectile.penetrate <= 0)
+            {
+                Projectile.Kill();
+            }
+            else
+            {
+                Collision.HitTiles(Projectile.position, Projectile.velocity, Projectile.width, Projectile.height);
+                SoundEngine.PlaySound(SoundID.Item10, Projectile.position);
+
+                // If the projectile hits the left or right side of the tile, reverse the X velocity
+                if (Math.Abs(Projectile.velocity.X - oldVelocity.X) > float.Epsilon)
+                {
+                    Projectile.velocity.X = -oldVelocity.X;
+                }
+
+                // If the projectile hits the top or bottom side of the tile, reverse the Y velocity
+                if (Math.Abs(Projectile.velocity.Y - oldVelocity.Y) > float.Epsilon)
+                {
+                    Projectile.velocity.Y = -oldVelocity.Y;
+                }
+            }
+
+            return false;
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            // Draws an afterimage trail.
+
+            Texture2D texture = TextureAssets.Projectile[Type].Value;
+
+            Vector2 drawOrigin = new Vector2(texture.Width * 0.5f, Projectile.height * 0.5f);
+            for (int k = Projectile.oldPos.Length - 1; k > 0; k--)
+            {
+                Vector2 drawPos = (Projectile.oldPos[k] - Main.screenPosition) + drawOrigin + new Vector2(0f, Projectile.gfxOffY);
+                Color color = Projectile.GetAlpha(lightColor) * ((Projectile.oldPos.Length - k) / (float)Projectile.oldPos.Length);
+                Main.EntitySpriteDraw(texture, drawPos, null, color, Projectile.rotation, drawOrigin, Projectile.scale, SpriteEffects.None, 0);
+            }
+
+            return true;
+        }
+
+        public override void OnKill(int timeLeft)
+        {
+            // This code and the similar code above in OnTileCollide spawn dust from the tiles collided with. SoundID.Item10 is the bounce sound you hear.
+            Collision.HitTiles(Projectile.position + Projectile.velocity, Projectile.velocity, Projectile.width, Projectile.height);
+            SoundEngine.PlaySound(SoundID.Item10, Projectile.position);
+        }
+    }
+}
