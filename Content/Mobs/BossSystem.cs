56bfbcb39af20bd8866fd93590085f729afb7b2e
DominicBellisario <dlb5102@g.rit.edu>
2025-11-14 17:07:34 -0500

    old gunslinger sells new weapons when paul is dead
    *not really.  the paulDead tag needs to be set in paul's script*

---
+++ Content/Mobs/BossSystem.cs
@@ -0,0 +1,31 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+
+namespace TheGoodTheBadAndTheIntoxicated.Content.Mobs
+{
+    public class BossSystem : ModSystem
+    {
+        public static bool paulDead = false;
+
+        public override void OnWorldLoad()
+        {
+            paulDead = false;
+        }
+
+        public override void OnWorldUnload()
+        {
+            paulDead = false;
+        }
+
+        public override void SaveWorldData(TagCompound tag)
+        {
+            tag["PaulDead"] = paulDead;
+        }
+
+        public override void LoadWorldData(TagCompound tag)
+        {
+            paulDead = tag.GetBool("PaulDead");
+        }
+    }
+}
